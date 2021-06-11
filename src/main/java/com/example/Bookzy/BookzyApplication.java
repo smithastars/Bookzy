ddbec21ce9f7b98c4457cc44a7b69047b88a31e1
smithastars <smithastars@gmail.com>
2021-06-11 14:34:28 -0500

    troubleshoot

--- src/main/java/com/example/Bookzy/BookzyApplication.java
+++ src/main/java/com/example/Bookzy/BookzyApplication.java
@@ -2,13 +2,19 @@ package com.example.Bookzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class BookzyApplication extends SpringBootServletInitializer{
+	@Override  
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
+	{  
+		return application.sources(BookzyApplication.class);  
+	}  
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(BookzyApplication.class, args);
-//	}
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyApplication.class, args);
+	}
 
 }
