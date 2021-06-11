81a7cc9f373b85433876be43656417b4bf05b5aa
smithastars <smithastars@gmail.com>
2021-06-11 12:02:20 -0500

    add tomcat config

--- src/main/java/com/example/Bookzy/BookzyApplication.java
+++ src/main/java/com/example/Bookzy/BookzyApplication.java
@@ -2,9 +2,10 @@ package com.example.Bookzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookzyApplication {
+public class BookzyApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookzyApplication.class, args);
